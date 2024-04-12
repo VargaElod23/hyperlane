dd7bee0de731a1f079ab6f13a4008bb1d6d88d39
VargaElod23 <vargaelod23@gmail.com>
2024-04-12 13:20:12 +0200

    adds taraxa testnet support

--- src/consts/chains.ts
+++ src/consts/chains.ts
@@ -1,31 +1,32 @@
-import { ChainMap, ChainMetadata } from '@hyperlane-xyz/sdk';
+import { ChainMap, ChainMetadata, ExplorerFamily } from '@hyperlane-xyz/sdk';
+import { ProtocolType } from '@hyperlane-xyz/utils';
 
 // A map of chain names to ChainMetadata
 // Chains can be defined here, in chains.json, or in chains.yaml
 // Chains already in the SDK need not be included here unless you want to override some fields
 // Schema here: https://github.com/hyperlane-xyz/hyperlane-monorepo/blob/main/typescript/sdk/src/metadata/chainMetadataTypes.ts
 export const chains: ChainMap<ChainMetadata & { mailbox?: Address }> = {
-  // mycustomchain: {
-  //   protocol: ProtocolType.Ethereum,
-  //   chainId: 123123,
-  //   domainId: 123123,
-  //   name: 'mycustomchain',
-  //   displayName: 'My Chain',
-  //   nativeToken: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  //   rpcUrls: [{ http: 'https://mycustomchain-rpc.com' }],
-  //   blockExplorers: [
-  //     {
-  //       name: 'MyCustomScan',
-  //       url: 'https://mycustomchain-scan.com',
-  //       apiUrl: 'https://api.mycustomchain-scan.com/api',
-  //       family: ExplorerFamily.Etherscan,
-  //     },
-  //   ],
-  //   blocks: {
-  //     confirmations: 1,
-  //     reorgPeriod: 1,
-  //     estimateBlockTime: 10,
-  //   },
-  //   logoURI: '/logo.svg',
-  // },
+  taraxa: {
+    protocol: ProtocolType.Ethereum,
+    chainId: 842,
+    domainId: 842,
+    name: 'taraxa',
+    displayName: 'Taraxa Testnet',
+    nativeToken: { name: 'Taraxa', symbol: 'TARA', decimals: 18 },
+    rpcUrls: [{ http: 'https://rpc.testnet.taraxa.io' }],
+    blockExplorers: [
+      {
+        name: 'Taraxa Explorer',
+        url: 'https://testnet.explorer.taraxa.io',
+        apiUrl: 'https://testnet.explorer.taraxa.io/api',
+        family: ExplorerFamily.Other,
+      },
+    ],
+    blocks: {
+      confirmations: 1,
+      reorgPeriod: 1,
+      estimateBlockTime: 4,
+    },
+    logoURI: '/logos/taraxa.svg',
+  },
 };
--- src/consts/tokens.ts
+++ src/consts/tokens.ts
@@ -1,9 +1,36 @@
-import { WarpCoreConfig } from '@hyperlane-xyz/sdk';
+import { TokenStandard, WarpCoreConfig } from '@hyperlane-xyz/sdk';
 
 // A list of Warp UI token configs
 // Tokens can be defined here, in tokens.json, or in tokens.yaml
 // The input here is typically the output of the Hyperlane CLI warp deploy command
 export const tokenConfigs: WarpCoreConfig = {
-  tokens: [],
+  tokens: [
+    {
+      chainName: 'taraxa',
+      standard: TokenStandard.EvmHypNative,
+      name: 'Taraxa',
+      symbol: 'TARA',
+      decimals: 18,
+      addressOrDenom: '0x263CE75A3D73e3657aa39c0aed332f1e0668DDa1',
+      connections: [
+        {
+          token: 'ethereum|alfajores|0xe2022Fb10F91014D0087BEdDA9d279b3Db57d173',
+        },
+      ],
+    },
+    {
+      chainName: 'alfajores',
+      standard: TokenStandard.EvmHypSynthetic,
+      name: 'Taraxa',
+      symbol: 'TARA',
+      decimals: 18,
+      addressOrDenom: '0xe2022Fb10F91014D0087BEdDA9d279b3Db57d173',
+      connections: [
+        {
+          token: 'ethereum|taraxa|0x263CE75A3D73e3657aa39c0aed332f1e0668DDa1',
+        },
+      ],
+    },
+  ],
   options: {},
 };
